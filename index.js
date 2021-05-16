f768c2a3e23722522e370ad4a4b76b4e54ca9f37
Ryota <n.ryo950911@gmail.com>
2021-05-16 20:32:15 +0900

    0516commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const json_data = [
+    {"ja":"こんにちは","ch":"你好"}
+];
+
+const Jindex =JSON.stringify(json_data);
+
+const Sindex = localStorage.setItem('json_data',Jindex);
+
+
+const Gindex = localStorage.getItem('json_data');
+const lang_ja = JSON.parse(Gindex);
+
+
+//const new_word = {"ja":"ありがとう","ch":"谢谢"};
+//json_data.push(new_word);
+
+
+/*-----------------------------------------------*/
+
+let add_ja = document.getElementById('add_ja');
+let add_ch = document.getElementById('add_ch');
+let save = document.getElementById('save');
+let display_ja = document.getElementById('display_ja');
+let display_ch = document.getElementById('display_ch}');
+
+save.addEventListener('click',() =>{
+    var new_word = {"ja":`${add_ja.value}`,"ch":`${add_ch.value}`};
+
+    json_data.push(new_word);
+    display_ja.textContent = JSON.parse(JSON.stringify(json_data[-1+json_data.length].ja));
+    display_ch.textContent = JSON.parse(JSON.stringify(json_data[-1+json_data.length].ch));
+});
